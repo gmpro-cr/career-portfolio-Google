319f11127be904f699a280a393c0f277c88cdc94
Mahalegaurav <mahalegauravk@gmail.com>
2026-05-17 17:09:51 +0530

    Fix horizontal scrolling overflow

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col bg-white">
+    <div className="min-h-screen flex flex-col bg-white overflow-x-hidden">
       <Navbar />
       <main className="flex-1">
         {children}
